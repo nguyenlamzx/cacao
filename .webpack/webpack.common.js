6def80a2fbe076389d703ed33592c82aaa21c0b8
nguyenlamzx <nguyenlamzx@outlook.com>
2018-08-14 04:06:11 +0700

    webpack + hbs (#1)

---
+++ .webpack/webpack.common.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+const project = require('../project');
+
+module.exports = {
+  entry: {
+    polyfills: project.src('polyfills.js'),
+    app: project.src('app.js'),
+  },
+  plugins: [
+    new CleanWebpackPlugin([
+      project.dist()
+    ]),
+    new webpack.HotModuleReplacementPlugin(),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast 
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
+    }),
+    
+    // Friendly webpack errors
+    new FriendlyErrorsWebpackPlugin(),
+
+    // prints more readable module names in the browser console on HMR updates
+    new webpack.NamedModulesPlugin(),
+  ],
+  output: {
+    filename: '[name].js',
+    path: project.dist(),
+    publicPath: './',
+  },
+  resolve: {
+    extensions: ['.hbs', '.html', '.ts', '.js', '.yaml', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: project.src(),
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          project.dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          // 'postcss-loader',
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     config: {
+          //       path: './webpack/',
+          //     },
+          //   },
+          // },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('node-sass'),
+              fiber: require('fibers'),
+            }
+          },
+        ],
+      },
+    ]
+  },
+};
---
+++ .webpack/webpack.dev.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const project = require('../project');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    publicPath: '/',
+    contentBase: project.static(),
+    hot: true,
+    inline: true,
+    overlay: true,
+    // noInfo: true,
+    before(app) {
+      app.get('/', (req, res) => {
+        res.send(fs.readFileSync(project.src('index.html'), 'utf-8'));
+      });
+    },
+  },
+  recordsInputPath: path.join(__dirname, 'records.json'),
+  recordsOutputPath: path.join(__dirname, 'newRecords.json')
+});
