bbbe80fbacfe8871f2c4d0bd80b7f6ea7d72da96
Lam Nguyen <nguyenlamzx@outlook.com>
2018-08-30 09:58:23 +0700

    update babel

---
+++ .babelrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "env": {
+    "production": {
+      "plugins": [
+        "babel-plugin-transform-classes",
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            "helpers": true,
+            "polyfill": false,
+            "regenerator": false,
+            "moduleName": "babel-runtime"
+          }
+        ],
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-syntax-import-meta",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-json-strings",
+        "@babel/plugin-transform-flow-strip-types",
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-transform-strict-mode",
+        [
+          "babel-plugin-transform-builtin-extend",
+          {
+            "globals": [
+              "Error",
+              "Array"
+            ]
+          }
+        ]
+      ]
+    }
+  },
+  "presets": [
+    [
+      "@babel/preset-env",
+      "@babel/typescript",
+      {
+        "useBuiltIns": "usage"
+      }
+    ]
+  ]
+}
